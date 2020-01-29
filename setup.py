9330e9da9384e4add4416cbe2e9f6099c4f4ef1c
obinym <48391916+obinym@users.noreply.github.com>
2020-01-29 13:21:57 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mws_shipping_rest_api',
+    version='1.0.0',
+    url='https://github.com/obinym/mws_shipping_rest_api.git',
+    author='obiny',
+    author_email='obiny@com',
+    description='see above',
+    packages=find_packages(),    
+    install_requires=[],
+)
