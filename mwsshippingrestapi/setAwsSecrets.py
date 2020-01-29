82d78f341076b80722689361e981254ac4838239
obinym <48391916+obinym@users.noreply.github.com>
2020-01-29 16:32:52 +0100

    Update setAwsSecrets.py

--- mwsshippingrestapi/setAwsSecrets.py
+++ mwsshippingrestapi/setAwsSecrets.py
@@ -1,6 +1,6 @@
 import os
 def init():
-  os.environ[mws_secret_key] = '123'
-  os.environ[mws_access_key] = '123'
-  os.environ[mws_account_id] = '123'
+  os.environ['mws_secret_key'] = '123'
+  os.environ['mws_access_key'] = '123'
+  os.environ['mws_account_id'] = '123'
   
