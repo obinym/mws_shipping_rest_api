d2f9dd1c7d6b50a9de032975e9d8be4d03561bcd
obinym <48391916+obinym@users.noreply.github.com>
2020-01-29 16:13:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     description='see above',
     packages=['mwsshippingrestapi'],
     include_package_data=True,
-    install_requires=['flask','connexion'],
+    install_requires=['flask','connexion[swagger-ui]'],
 )
