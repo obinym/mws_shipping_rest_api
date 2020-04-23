73e48ec75d6a3369c0def37d47f2578c716a8078
obinym <48391916+obinym@users.noreply.github.com>
2020-04-23 18:39:42 +0200

    Added ShippingServiceOfferId to create_shipment
    looks like MWS has made an optional parameter a requirement ... nice

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mwsshippingrestapi',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/obinym/mws_shipping_rest_api.git',
     author='obinym',
     author_email='obiny@com',
